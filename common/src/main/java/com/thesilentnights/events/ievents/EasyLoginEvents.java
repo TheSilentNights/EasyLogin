65a161a56f0d83a9a2601ba42eac838d288a599c
TheSilentNights <yangsichen080320@outlook.com>
2025-11-06 21:15:35 +0800

    refactor(events): 修改登录事件为循环事件
    - 将 ON_LOGIN事件类型从 EventResult 改为 Loop- 将 ON_LOGOUT 事件类型从 EventResult 改为 Loop
    - 添加注释说明使用循环处理事件

--- common/src/main/java/com/thesilentnights/events/ievents/EasyLoginEvents.java
+++ common/src/main/java/com/thesilentnights/events/ievents/EasyLoginEvents.java
@@ -6,8 +6,9 @@ import dev.architectury.event.EventFactory;
 import net.minecraft.server.level.ServerPlayer;
 
 public class EasyLoginEvents {
-    public static final Event<PlayerLoginEvent> ON_LOGIN = EventFactory.createEventResult();
-    public static final Event<PlayerLogoutEvent> ON_LOGOUT = EventFactory.createEventResult();
+    //use loop!
+    public static final Event<PlayerLoginEvent> ON_LOGIN = EventFactory.createLoop();
+    public static final Event<PlayerLogoutEvent> ON_LOGOUT = EventFactory.createLoop();
 
     public interface PlayerLoginEvent{
         void onLogin(PlayerAccount account, ServerPlayer serverPlayer);
