cd7e4a45aacc3f27410e555ccf21fef5a46e4cb4
TheSilentNights <yangsichen080320@outlook.com>
2026-04-16 21:09:40 +0800

    refactor(commands): 重构注册命令的构建逻辑以提高可读性
    将命令构建从链式调用改为分步变量定义，使代码结构更清晰

--- src/main/java/cn/thesilentnights/easylogin/commands/Registrar.java
+++ src/main/java/cn/thesilentnights/easylogin/commands/Registrar.java
@@ -2,6 +2,8 @@ package cn.thesilentnights.easylogin.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 
 import cn.thesilentnights.easylogin.service.LoginService;
@@ -10,19 +12,19 @@ import net.minecraft.commands.Commands;
 
 public class Registrar implements ICommands {
 
-    @Override
-    public void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(
-                Commands.literal("register")
-                        .then(
-                                Commands.argument("password", StringArgumentType.string())
-                                        .then(
-                                                Commands.argument("repeat", StringArgumentType.string())
-                                                        .executes((CommandContext<CommandSourceStack> context) ->
-                                                                LoginService.register(context) ? 1 : 0
-                                                        )
-                                        )
-                        )
-        );
-    }
+        @Override
+        public void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+                var register = Commands.literal("register");
+                var password = Commands.argument("password", StringArgumentType.string());
+                var repeat = Commands.argument("repeat", StringArgumentType.string());
+
+                repeat.executes(
+                        (CommandContext<CommandSourceStack> context) -> LoginService.register(context)
+                                ? 1
+                                : 0
+                );
+
+                //serialize
+                dispatcher.register(register.then(password.then(repeat)));
+        }
 }
